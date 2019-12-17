e58ec4d1fa1879215aaa16d9d6f6baaddb9dbacd
Luis Tupa <luis.tupa@orbis.com.pe>
2019-12-17 17:46:47 -0500

    feat(app): add base config

---
+++ app/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const rootPath = path.join(__dirname, './client');
+
+const publicPath = '/';
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    'app': path.join(rootPath, 'src/index.tsx')
+  },
+  output: {
+    path: path.join(rootPath, 'dist'),
+    publicPath
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@app': rootPath
+    }
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader'
+          }
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'UND | React Hooks',
+      hash: true,
+      template: path.join(rootPath, 'public/index.html')
+    })
+  ]
+}
